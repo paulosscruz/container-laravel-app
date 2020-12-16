ddb88062895ecf36a37c2eac1b4ed22a072193d9
Paulo Cruz <paulo.cruz@outlook.com>
2020-12-16 14:14:06 -0300

    Remove option to clone repository...

--- .docker/application/entrypoint-application.sh
+++ .docker/application/entrypoint-application.sh
@@ -7,27 +7,22 @@ then
 	rm -rf /var/www/.git
 	rm -f /var/www/README*
 
-	if [ -z $APPLICATION_REPOSITORY]
-	then
-		composer create-project laravel/laravel temp-project
-		cd /var/www/temp-project
-		git init
-		cd ..
-	else
-		git clone $APPLICATION_REPOSITORY temp-project
-		composer -d /var/www/temp-project install	
-	fi
-	
+	composer create-project laravel/laravel temp-project
+	cd /var/www/temp-project
+	git init
+	cd.. 
+
 	rm -f /var/www/temp-project/.env
         rm -f /var/www/temp-project/docker-compose.yml
 	rm -f /var/www/temp-project/docker.env
 	rm -rf /var/www/temp-project/.docker
 	
-	mv /var/www/temp-project/* .
+	mv /var/www/temp-project/* /var/www/
 	rm -rf /var/www/temp-project
 	php artisan key:generate
 
 else
+	composer install
 	php artisan key:generate
 fi
 
